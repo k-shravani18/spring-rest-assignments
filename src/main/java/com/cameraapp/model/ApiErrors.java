6a5d3e4ca0374f15bfa15a156187a1322971441a
Karupakala Shravani <karupakalashravani@gmail.com>
2022-04-28 16:19:53 +0530

    initial commit

---
+++ src/main/java/com/cameraapp/model/ApiErrors.java
@@ -0,0 +1,20 @@
+package com.cameraapp.model;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ApiErrors {
+    LocalDateTime timestamp;
+    HttpStatus status;
+    int statusCode;
+    String error;
+    List<String> message;
+}
